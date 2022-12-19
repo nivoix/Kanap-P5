7c210db7b99cafa84b3e9109ad0cda048d4ff530
nivoix <jnivoix@yahoo.fr>
2022-12-19 11:54:18 +0100

    recover choice user

--- front/js/product.js
+++ front/js/product.js
@@ -6,25 +6,52 @@ const urlSearchParams = new URLSearchParams(queryString_url_id);
 
 const id = urlSearchParams.get("id");
 
+
 // récuperation de l'article
 fetch(`http://localhost:3000/api/products/${id}`)
     .then((res) => res.json())
+        
     .then((data) => {
-        console.log(data);        
+        console.log(data)       
         document.querySelector('.item__img').innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`
+        
         document.querySelector('.item__content__titlePrice').innerHTML = `<h1 id="title">${data.name}</h1>
         <p>Prix : <span id="price">${data.price}</span>€</p>`
+        
         document.querySelector('#description').innerHTML = `<p id="description">${data.description}</p>`
-        let colorChoise = ``
+        
+       
         for(let color of data.colors) {
-            colorChoise +=`
-            <option value="${color}">${color}</option>`
-            console.log(color);
-            document.getElementById("colors").innerHTML = colorChoise
-
+            document.getElementById("colors").innerHTML  += `
+            <option value="${color}">${color}</option>` 
         }
+            
     })
     .catch((error) => {
         return error;
     });
 
+document.getElementById('addToCart').addEventListener('click', () => {
+checkInput ()  
+})
+// récup info de saisie
+function checkInput () {
+    let colorChoice = document.getElementById('colors').value
+    if(colorChoice === "") {
+        console.log('choisissez une couleur');
+    }
+
+    console.log(colorChoice);
+    let quantity = document.getElementById('quantity').value
+    if(quantity < 1 || quantity >99) {
+        console.log('veuillez saisir une bonne quantité entre 1 et 100')
+    }
+    console.log(quantity);
+    if (colorChoice != "" && quantity > 1 && quantity < 100) {
+        addToLocalStorage()
+    }
+}
+
+function addToLocalStorage () {
+    console.log('ajout dans le ls');
+}
