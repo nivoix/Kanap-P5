4a2c1522ef8ab5d55a4f3ba5bb7fbcdd16bf4cfb
nivoix <jnivoix@yahoo.fr>
2022-12-20 02:18:58 +0100

    add a product to local storage

--- front/js/product.js
+++ front/js/product.js
@@ -7,12 +7,12 @@ const urlSearchParams = new URLSearchParams(queryString_url_id);
 const id = urlSearchParams.get("id");
 
 
+
 // récuperation de l'article
 fetch(`http://localhost:3000/api/products/${id}`)
     .then((res) => res.json())
         
-    .then((data) => {
-        console.log(data)       
+    .then((data) => {  
         document.querySelector('.item__img').innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`
         
         document.querySelector('.item__content__titlePrice').innerHTML = `<h1 id="title">${data.name}</h1>
@@ -20,38 +20,55 @@ fetch(`http://localhost:3000/api/products/${id}`)
         
         document.querySelector('#description').innerHTML = `<p id="description">${data.description}</p>`
         
-       
         for(let color of data.colors) {
             document.getElementById("colors").innerHTML  += `
             <option value="${color}">${color}</option>` 
-        }
-            
+        }     
     })
     .catch((error) => {
         return error;
     });
 
+
 document.getElementById('addToCart').addEventListener('click', () => {
-checkInput ()  
+    checkInput ()
+        
 })
-// récup info de saisie
+
+// récup infos de saisie et verif des éléments saisis
 function checkInput () {
     let colorChoice = document.getElementById('colors').value
     if(colorChoice === "") {
         console.log('choisissez une couleur');
     }
-
     console.log(colorChoice);
+
     let quantity = document.getElementById('quantity').value
     if(quantity < 1 || quantity >99) {
         console.log('veuillez saisir une bonne quantité entre 1 et 100')
     }
     console.log(quantity);
-    if (colorChoice != "" && quantity > 1 && quantity < 100) {
-        addToLocalStorage()
+    if (colorChoice != "" && quantity >= 1 && quantity < 100) {
+        let productChoice = [id, colorChoice, quantity]
+        addToLocalStorage(productChoice)
     }
 }
 
-function addToLocalStorage () {
-    console.log('ajout dans le ls');
+function saveBasket(basket) {
+    localStorage.setItem("basket", JSON.stringify(basket));
 }
+
+function getBasket() {
+    let basket = localStorage.getItem("basket");
+    if (basket == null) {
+        return [];
+    }else{
+        return JSON.parse(basket)
+    }
+}
+function addToLocalStorage (productChoice) {
+    let basket = getBasket();
+    basket.push(productChoice);
+    saveBasket(basket);
+}
+
