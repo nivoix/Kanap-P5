1cca838f1113a159275625c1ee8bbc1afb1f38c4
nivoix <jnivoix@yahoo.fr>
2023-01-04 03:50:57 +0100

    delete product ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -14,6 +14,8 @@ function saveBasket(basket) {
     localStorage.setItem("basket", JSON.stringify(basket));
 }
 
+
+
 // changement de quantité d'un produit
 function changeQuantity(){
     let lastQuantity =  document.querySelectorAll(".itemQuantity");
@@ -59,85 +61,83 @@ function totalPriceFromBasket () {
     let basket = getBasket();
     let price = 0;
     for(let product of basket) {
-        console.log(product);
-        console.log(products);
         for(let p = 0; p < products.length; p++) {
             if(product.id === products[p]._id){
                 price += products[p].price * product.quantity;
-                console.log(products[p].price);
-                console.log(product.quantity);
                 let totalPrice = document.getElementById('totalPrice');
                 totalPrice.textContent = price; 
             }
         }
     }
 }
+// suprimer un produit
+function removeProduct () {
+    let remove = document.getElementsByClassName('deleteItem');
+    for(let r = 0; r < remove.length; r++){
+        remove[r].addEventListener('click', () => {
+            let eArticle = remove[r].closest("article");
+            let eId = eArticle.getAttribute("data-id");
+            let eColor = eArticle.getAttribute("data-color");
+            let product = {
+                id: eId,
+                colorChoice: eColor,
+            }
+            deleteFromBasket(product)    
+        })
+    }
+}
 
+//suppression de produits dans LS
+function deleteFromBasket(product) {
+            let basket = getBasket();
+            basket = basket.filter(d => (d.id != product.id) || (d.colorChoice != product.colorChoice))
+            console.log(basket);
+            if(product = []){
+                localStorage.removeItem('basket');
+            }
+            location.reload();
+            saveBasket(basket);
+}
    
 fetch(`http://localhost:3000/api/products`)
 .then((res) => res.json())
 .then((data) => {
     products = data;
     for(let x = 0; x < basket.length; x++) {
-    let basket = getBasket();
-        for(let y = 0; y < data.length; y++){
-            if(basket[x].id === data[y]._id){
-            //prix total pour le produit en fonction de sa quantité: let totalPriceProduct = (parseInt(data[y].price) * parseInt(basket[x].quantity))
-            document.getElementById("cart__items").innerHTML +=
-                    `<article class="cart__item" data-id=${basket[x].id} data-color=${basket[x].colorChoice}>
-                        <div class="cart__item__img"><img src="${data[y].imageUrl}" alt="${data[y].description}"></div>
-                        <div class="cart__item__content">
-                            <div class="cart__item__content__description">
-                                <h2>${data[y].name}</h2>
-                                <p>${basket[x].colorChoice}</p>
-                                <p>${data[y].price}€</p>
-                            </div>
-                            <div class="cart__item__content__settings">
-                                <div class="cart__item__content__settings__quantity">
-                                    <p>Qté : </p>
-                                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${basket[x].quantity}">
+        let basket = getBasket();
+            for(let y = 0; y < data.length; y++){
+                if(basket[x].id === data[y]._id){
+                //prix total pour le produit en fonction de sa quantité: let totalPriceProduct = (parseInt(data[y].price) * parseInt(basket[x].quantity))
+                document.getElementById("cart__items").innerHTML +=
+                        `<article class="cart__item" data-id=${basket[x].id} data-color=${basket[x].colorChoice}>
+                            <div class="cart__item__img"><img src="${data[y].imageUrl}" alt="${data[y].description}"></div>
+                            <div class="cart__item__content">
+                                <div class="cart__item__content__description">
+                                    <h2>${data[y].name}</h2>
+                                    <p>${basket[x].colorChoice}</p>
+                                    <p>${data[y].price}€</p>
                                 </div>
-                                <div class="cart__item__content__settings__delete">
-                                    <br>
-                                    <p class="deleteItem">Supprimer</p>
+                                <div class="cart__item__content__settings">
+                                    <div class="cart__item__content__settings__quantity">
+                                        <p>Qté : </p>
+                                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${basket[x].quantity}">
+                                    </div>
+                                    <div class="cart__item__content__settings__delete">
+                                        <br>
+                                        <p class="deleteItem">Supprimer</p>
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                            </div>
-                          </article>`                             
+                              </article>`                             
+                }
             }
         }
-    }
 // changement de quantité d'un produit
     changeQuantity();
 // changement de quantité total de produit
     totalQuantityFromBasket();
 // prix total du panier
     totalPriceFromBasket();
+// sup product
+    removeProduct();
 })
-
-  
-  
-
-
-/* function deleteFromBasket(productChoice) {
-    let basket = getBasket();
-    basket = basket.filter(p => p.id != productChoice.id);
-    saveBasket();
-}
- */
-
-
-
-
-
-    
-
-    
-    
-        
-    
-    
-
-    
-
-
